dd37551ea2e682cb067f6e0a832477945936002e
djimi gilles <djimi@djimis-MacBook-Pro.local>
2023-05-08 07:24:09 +0200

    config: Add application helm chart.

--- terraform/app.tf
+++ terraform/app.tf
@@ -1,6 +1,26 @@
-# https://registry.terraform.io/providers/hashicorp/helm/latest/docs/resources/release
+#### Resources: https://registry.terraform.io/providers/hashicorp/helm/latest/docs/resources/release
 resource "helm_release" "doc_planner_app" {
     name       = var.docplanner_release_name
     chart      = "../k8s/docplanner"
     version    = var.docplanner_helmchart_version
+
+    set {
+    name  = "app.registry"
+    value = "noussydjimi/docplanner"
+  }
+
+    set {
+    name  = "app.front.tag"
+    value = "front"
+  }
+
+    set {
+    name  = "app.back.tag"
+    value = "back"
+  }
+
+    set {
+    name  = "app.domain"
+    value = ""
+  }
 }
